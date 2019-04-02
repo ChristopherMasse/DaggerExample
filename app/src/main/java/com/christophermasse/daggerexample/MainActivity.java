2660ea5efc6f1bade9ce2c2650d6587c72408105
Chris Masse (M) <christophermassegithub@gmail.com>
2019-04-02 17:18:47 -0400

    added comment

--- app/src/main/java/com/christophermasse/daggerexample/MainActivity.java
+++ app/src/main/java/com/christophermasse/daggerexample/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Comment
+    //Comment 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
